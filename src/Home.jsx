11c8fa1173a25a2a2d7d669f80a849890e0561de
Slevin Cordeiro <slevincordeiro@gmail.com>
2025-04-29 01:02:32 +0530

    added educational page

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,7 +12,7 @@ const cards = [
   { icon: "📰", title: "News", description: "Learn about the latest news on disasters in the world.", link: "/Pages_on_test/feedback.html" },
   { icon: "📊", title: "Dashboard", description: "Monitor Real-time conditions of your area.", link: "Pages_on_test/dashboard.html" },
   { icon: "📞", title: "Emergency Contact", description: "Reach out for urgent assistance and support.", link: "/Pages_on_test/emergency_contacts.html" },
-  { icon: "📚", title: "Educational Content", description: "Learn about how to mitigate through a disaster.", link: "/Pages_on_test/help.html" },
+  { icon: "📚", title: "Educational Content", description: "Learn about how to mitigate through a disaster.", link: "/Pages_on_test/emergencyalertpage.html" },
 ];
 
 const Home = () => {
